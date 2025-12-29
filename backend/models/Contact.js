1d57673431b83e12cf71ebf3888b59bbc70448f2
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2025-12-29 18:19:59 +0100

    Contact.js

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const ContactSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: false }, // <--- Changed to false
+  phone: { type: String, required: true }, // <--- THIS WAS MISSING!
+  email: { type: String, required: false }, // Optional as requested
   subject: { type: String, required: false },
   message: { type: String, required: true },
   date: { type: Date, default: Date.now },
