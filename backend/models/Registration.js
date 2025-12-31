cc59a4a313b7e2e35e6a199045fec4b1f9081f8b
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2025-12-31 18:51:22 +0100

    Registration.js

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -5,7 +5,19 @@ const RegistrationSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   role: { type: String, required: true },
   level: { type: String, required: true },
-  message: { type: String },
+  city: { type: String, default: "" },
+  assignedTo: { type: String, default: null },
+  status: { type: String, default: "Nouveau prospect" },
+  appointment: {
+    date: { type: Date, default: null },
+    note: { type: String, default: "" },
+  },
+  // 👇 NEW FIELD
+  assignedClass: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Class",
+    default: null,
+  },
   date: { type: Date, default: Date.now },
 });
 
